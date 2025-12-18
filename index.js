174b5d9355c7c106ad04f37b9e7362ef823cb444
Avinash Verma <verma@cbtarchitects.com>
2025-12-18 13:23:04 -0500

    Completed header, CV needs to be linked in

---
+++ index.js
@@ -0,0 +1,6 @@
+const award = document.querySelector(".award");
+award.addEventListener("click", () => {
+  window.open(
+    "https://www.sara-national.org/2023-national-student-design-awards"
+  );
+});
