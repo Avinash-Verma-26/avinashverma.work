46cf19ef8b3c4ca679aa1722809a237a14cb8b93
Avinash Verma <68919187+Avinash-Verma-26@users.noreply.github.com>
2026-01-26 21:58:29 -0500

    Fixing image view issues

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -1,12 +1,9 @@
 import "./Home.css";
+import headshot from "../../assets/headshot.jpeg";
 const Home = () => {
   return (
     <div className="home-section">
-      <img
-        className="headshot"
-        src="src/assets/headhot.jpeg"
-        alt="Profile picture"
-      />
+      <img className="headshot" src={headshot} alt="Profile picture" />
     </div>
   );
 };
