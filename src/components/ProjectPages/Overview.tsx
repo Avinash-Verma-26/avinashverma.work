073201d80c7e3dd7ffdb59eb6f1f864ff5496467
Avinash Verma <68919187+Avinash-Verma-26@users.noreply.github.com>
2026-02-26 20:18:32 +0530

    overview updated

--- src/components/ProjectPages/Overview.tsx
+++ src/components/ProjectPages/Overview.tsx
@@ -1,7 +1,23 @@
 import React from "react";
+import "./Overview.css";
+import overviewGraphic from "../../assets/cbt-project-images/overview-graphic.jpg";
 
 const Overview = () => {
-  return <div>Overview</div>;
+  return (
+    <div className="overview-container">
+      <div className="summary">
+        CBT Tools is an integrated platform that connects custom{" "}
+        <b>Revit Plugin, a backend data layer, and a React web dashboard</b> to
+        make model activity and tool usage more visible. The Revit plugins
+        capture telemetry and support model-related workflows, the backend
+        stores and organizes that information, and the web UI turns it into a
+        clear, readable view of how teams and models are performing. Together,
+        the system creates a centralized way to monitor usage, understand model
+        behavior, and support better BIM decision-making.
+      </div>
+      <img className="infographic" src={overviewGraphic} alt="overview image" />
+    </div>
+  );
 };
 
 export default Overview;
